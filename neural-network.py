87024cc13f9cb0b03d939cddf52394b28ba2942d
Jack Kurtz <43488541+jbkurtz@users.noreply.github.com>
2019-07-17 18:30:14 -0400

    Completed training function.

--- neural-network.py
+++ neural-network.py
@@ -24,8 +24,35 @@ class neuralNetwork:
         self.who = (numpy.random.rand(self.onodes, self.hnodes) - 0.5)
 
     # Refine the weights in the network after using a training set
-    def train():
-        pass
+    def train(self, inputs_list, targets_list):
+        # Convert inputs list into 2D Array
+        inputs = numpy.array(inputs_list, ndmin=2) .T
+
+        # Calculate signal into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # Calculate signal emerging from hidden layer
+        hidden_outputs = self.activation(hidden_inputs)
+
+        # Calculate signal into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # Calculate signal emerging from final output layer
+        final_outputs = self.activation(final_inputs)
+
+        # Convert targets list into 2D Array
+        targets = numpy.array(targets_list, ndmin=2) .T
+
+        # Calculate error
+        output_errors = targets - final_outputs
+
+        # Calculate hidden error
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        # Update weights for links between hidden and output layers
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1 - final_outputs)), numpy.transpose(hidden_outputs))
+
+        # Update the weights for links between input and hidden layers
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
+
 
     # Activation function, currently a sigmoid function
     def activation(self, value):
