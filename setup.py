80ccfdb4cae932705ae58105363328a7df97d7f6
Jack Kurtz <john.kurtz@accenture.com>
2019-08-06 09:22:00 -0400

    Added larger datasets

--- setup.py
+++ setup.py
@@ -3,3 +3,4 @@ import subprocess
 subprocess.call(['pip', 'install', 'numpy'])
 subprocess.call(['pip', 'install', 'scipy'])
 subprocess.call(['pip', 'install', 'matplotlib'])
+subprocess.call(['unzip', 'mnist_dataset/mnist_train.zip', '-d', 'mnist_dataset/'])
