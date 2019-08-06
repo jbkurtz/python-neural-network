eb88562f14e910e0bf8f971c08e5de9307f8f059
Jack Kurtz <john.kurtz@accenture.com>
2019-08-06 09:59:36 -0400

    Added continuous user interaction

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ __Author:__ Jack Kurtz (jbkurtz)
 This is project is my attempt at a personal introduction to neural networks. I will be following along with '[Make Your Own Neural Network](https://makeyourownneuralnetwork.blogspot.com/)' by Tariq Rashid. The goal of this project is to design and build a neural network to identify and categorize handwritten numbers in Python.
 
 # Usage  
-After cloning the repository, run setup.py to install the needed dependencies. This should only be needed once.
+After cloning the repository, run `python setup.py` to install the needed dependencies and unzip the datasets. This should only be needed once.
 
-It can be run by using `python main.py`. It currently uses the images found in mnist_dataset/images for training and testing.
+It can be run by using `python main.py`.  
+
+Once the network has been trained, the user can provide 3 options.  
+  - Enter a number, 0-9, to test the corresponding image from mnist_dataset/images/test  
+  - Enter 'all' to test against all 10 images in mnist_dataset/images/test  
+  - Enter 'q' to quit the program  
--- main.py
+++ main.py
@@ -2,48 +2,86 @@ import numpy
 import matplotlib.pyplot
 import neuralnetwork as nn
 
-# Number of nodes
-# Number of input pixels
-input_nodes = 784
-# Arbitrary 
-hidden_nodes = 100
-# Number of options for output
-output_nodes = 10 
+def train(network, output_nodes):
+    # Train the network
+    with open('mnist_dataset/mnist_train.csv') as fp:
+        training_data = fp.readlines()
 
-learning_rate = 0.3
+    for record in training_data:
+        all_values = record.split(',')
+        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.1
+        targets = numpy.zeros(output_nodes) + 0.1
+        targets[int(all_values[0])] = 0.99
+        network.train(inputs, targets)
 
-network = nn.neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+def test_all(network):
+    # Test the network
+    with open('mnist_dataset/mnist_test_10.csv') as fp:
+        test_data = fp.readlines()
 
-# Train the network
-with open('mnist_dataset/mnist_train.csv') as fp:
-    training_data = fp.readlines()
+    scorecard = []
 
-for record in training_data:
-    all_values = record.split(',')
-    inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.1
-    targets = numpy.zeros(output_nodes) + 0.1
-    targets[int(all_values[0])] = 0.99
-    network.train(inputs, targets)
+    for record in test_data:
+        all_values = record.split(',')
+        print('Correct label: ', all_values[0])
+        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.1
+        outputs = network.query(inputs)
+        print("Network's answer: ", numpy.argmax(outputs))
+        if( int(all_values[0]) == numpy.argmax(outputs) ):
+            scorecard.append(1)
+        else:
+            scorecard.append(0)
 
-# Test the network
-with open('mnist_dataset/mnist_test_10.csv') as fp:
-    test_data = fp.readlines()
+    print('Scorecard: ', scorecard)
 
-scorecard = []
+    scorecard_array = numpy.asarray(scorecard)
 
-for record in test_data:
-    all_values = record.split(',')
+    print('Performance: ', scorecard_array.sum() / scorecard_array.size * 100.0, "%\n")
+
+def test(network, img):
+    with open('mnist_dataset/mnist_test_10.csv') as fp:
+        test_data = fp.readlines()
+
+    all_values = test_data[img].split(',')
     print('Correct label: ', all_values[0])
     inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.1
     outputs = network.query(inputs)
-    print("Network's answer: ", numpy.argmax(outputs))
-    if( int(all_values[0]) == numpy.argmax(outputs) ):
-        scorecard.append(1)
-    else:
-        scorecard.append(0)
+    print("Network's answer: ", numpy.argmax(outputs), "\n")
 
-print('Scorecard: ', scorecard)
+def main():
+    # Number of nodes
+    # Number of input pixels
+    input_nodes = 784
+    # Arbitrary 
+    hidden_nodes = 100
+    # Number of options for output
+    output_nodes = 10 
 
-scorecard_array = numpy.asarray(scorecard)
+    learning_rate = 0.3
 
-print('Performance: ', scorecard_array.sum() / scorecard_array.size)
+    network = nn.neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
+    print("Training network! May take some time...")
+    train(network, output_nodes)
+
+    print("Test the network!")
+    print("Options: ")
+    print("    Enter a number, 0-9, to test the image from mnist_dataset/images/test")
+    print("    Enter 'all' to test against all images in mnist_dataset/images/test")
+    print("    Type 'q' to quit")
+
+    choice = input("Input: ")
+
+    while choice is not 'q':
+        try:
+            input_num = int(choice)
+            test(network, input_num)
+        except ValueError:
+            if choice == 'all':
+                test_all(network)
+            else:
+                print("Error - unknown command")
+
+        choice = input("Input: ")
+
+main()
